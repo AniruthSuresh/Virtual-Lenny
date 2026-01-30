dd5b4f778c8d187e0d1508c841eeb04f87abe1b0
AniruthSuresh <aniruthzlatan@gmail.com>
2026-01-30 21:40:57 +0530

    feat : encoding all the chunks using the best model (ablation done earlier)

---
+++ src/encode-all-chunks.py
@@ -0,0 +1,14 @@
+import torch
+from sentence_transformers import SentenceTransformer
+import json
+
+model = SentenceTransformer("mixedbread-ai/mxbai-embed-large-v1", device="cuda") # best perf model 
+
+with open("../data/chunks/final_chunks.json") as f:
+    all_chunks = json.load(f)
+
+corpus_texts = [c['content'] for c in all_chunks]
+
+corpus_embs = model.encode(corpus_texts, convert_to_tensor=True, show_progress_bar=True)
+
+torch.save({"embeddings": corpus_embs, "chunks": all_chunks}, "../data/embedded/mxbai_corpus.pt")
